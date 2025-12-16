dc8d9e6e8ee6826cab93c3b0d43bb7e7945800ad
iyann1255 <tessbarca1981@gmail.com>
2025-12-16 12:24:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", "29655477"))
+API_HASH = os.getenv("API_HASH", "2de42d28d36e637c54d5571df5679b7d")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7714224903:AAEI8X6z_A34C5mDlOQcCaTXBkKnp5Q0uTs")
 
 # Assistant user account session string (wajib untuk join voice chat)
-ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "")
+ASSISTANT_SESSION = os.getenv("ASSISTANT_SESSION", "BQF-4awAOJEHp5bqDsdHGatIW-x-EGmq6moHeaE0QHyVHSWqP2z1TyoL_BvWbKTS4egfzBKbH3wbG9gOhWR350_OodFk5Ya5p1wFxBFjyTr2vPo547zazMa-Z1-Y9r0B-CkGG4iIBDME8GAYXZ0OHOaVv1HyTJ8gNJt3eUo2OQapEckXLmN9t2pIvZh2Af8IeZAts0vvpn2RJkmS93dokPFoGUKJ9LyHQ7E6fbrdGvq7TA-OFt45S-E5uk7coZE-fOVM3q-rK6S838QRXzjq_tKrcyyaRRmYD1xt98KZSb8YxyAapP2OlmQOaga2wvVyqmwRsWpaPSUycbBp85OWQqfOpWQwWwAAAAHn-8vxAA")
 
 # YouTube Data API (untuk search metadata, bukan streaming)
-YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY", "")
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY", "AIzaSyDUsnrgyghsrZ3HLErNes8SGX5mONi5vWk")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+OWNER_ID = int(os.getenv("OWNER_ID", "5504473114"))
